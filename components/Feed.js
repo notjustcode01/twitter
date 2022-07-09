ab28aecc49689b860aface982f901016e5044612
notjustcode01 <mohammedhabibullahi2004@gmail.com>
2022-07-09 17:48:54 +0100

    add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,28 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts =[
+    {
+      id: "1",
+      name: "Habib Mohammed",
+      username: "notjustcode01",
+      userImg: "https://lh3.googleusercontent.com/a-/AFdZucqvsyLh0w1-foe12qK0_04ZhQ2VfzdqMh-MTnTs=s504-p-rw-no",
+      img: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
+      text: "nice view",
+      timestamp: "2 hours ago"
+    },
+    {
+      id: "2",
+      name: "Habib Mohammed",
+      username: "notjustcode01",
+      userImg: "https://lh3.googleusercontent.com/a-/AFdZucqvsyLh0w1-foe12qK0_04ZhQ2VfzdqMh-MTnTs=s504-p-rw-no",
+      img: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60",
+      text: "wow",
+      timestamp: "2 days ago"
+    },
+  ]
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
         <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +32,9 @@ export default function Feed() {
             </div>
         </div>
         <Input/>
+        {posts.map((post)=>(
+          <Post key={post.id} post={post}/>
+        ))}
     </div>
   )
 }
